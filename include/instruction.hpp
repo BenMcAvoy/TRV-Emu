f37a2cedfd67bef576b286935ff82bd0cf9b0709
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-04-20 22:03:37 +0100

    chore: add todo notice

--- include/instruction.hpp
+++ include/instruction.hpp
@@ -1,6 +1,10 @@
 #ifndef INSTRUCTION_HPP
 #define INSTRUCTION_HPP
 
+// TODO: Create a central Instruction class
+// This is because currently we only have operation codes.
+// This will require research into how the RISC-V ISA works.
+
 // RISC-V 32I Base ISA opcodes
 enum OpCode {
     // R FMT - ALU
